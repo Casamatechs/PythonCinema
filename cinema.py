3cb43ec2fccdb60d2b659237028affae1875d1df
Álvaro Arranz <alvaro.arranz@outlook.com>
2020-03-10 17:06:32 +0100

    Exercise 3 Finished
    Need validation from the group

--- cinema.py
+++ cinema.py
@@ -1,3 +1,5 @@
+import string
+from random import random
 from typing import List
 
 from film import Film
@@ -7,17 +9,53 @@ from spectator import Spectator
 
 class Cinema:
 
-    __seats: List[Seat]
-    __film: Film
+    def __init__(self, film: Film, rows: int, cols: int):
+        self.__film = film
+        self.__allocated_seats = 0
+        self.__allocated_spectators = []
+        self.__max_seats = int(rows * cols)
+        self.__room = [[Seat(row + 1, col + 1) for col in range(cols)] for row in range(rows)]
 
-    def allocateSpectators(self, spectatorList: List[Spectator]):
+    def allocate_spectators(self, spectator_list: List[Spectator]):
+        num_rows = len(self.__room)
+        num_cols = len(self.__room[0])
+
+        for spectator in spectator_list:
+            if spectator.can_buy_film_ticket(self.__film):
+                allocated = False
+
+                while not allocated:
+                    row = random.randint(0, num_rows)
+                    col = random.randint(0, num_cols)
+                    allocated = self.__allocate_spectator(spectator, self.__room[row][col])
+
+    def __allocate_spectator(self, spectator: Spectator, seat: Seat):
+        result = False
+
+        if seat.check_availability():
+            seat.occupy_seat(spectator)
+            self.__allocated_spectators.append(spectator)
+            self.__allocated_seats = self.__allocated_seats + 1
+            result = True
+
+        return result
+
+    def get_allocated_spectators(self):
+        return self.__allocated_spectators
+
+    def show_seats(self):
+        num_cols = len(self.__room[0] * 7)
+        num_spaces = int((num_cols - len("SCREEN") - 2) / 2)
+
+        print("#" * num_cols + "\n#" + " " * num_spaces + "SCREEN" + " " * num_spaces + "#\n" + "#" * num_cols)
+        print("\n\n\n")
+        print('\n'.join([''.join([f' {seat.__str__()} ' for seat in row]) for row in self.__room]))
         return None
 
-    def getAllocatedSpectators(self):
-        return None
+    def get_seats(self):
+        list_seats = []
 
-    def showSeats(self):
-        return None
+        for row in self.__room:
+            list_seats = list_seats + row
 
-    def getSeats(self):
-        return None
+        return list_seats
